38f874f502174286c5affcbcdfbc4b657c84926a
Pieter Michels <pieter@noort.be>
2011-09-02 16:13:57 +0200

    cleaning up

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ git clone git://github.com/pierot/termieter.git ~/.termieter
 
 echo "Linking"
 
-  echo -e "\t.bash_profile"
+  echo "\t.bash_profile"
   ln -sf ~/.termieter/bash_profile ~/.bash_profile
 
-  echo -e "\t.gitconfig"
+  echo "\t.gitconfig"
   ln -sf ~/.termieter/gitconfig ~/.gitconfig
 
-  echo -e "\t.screenrc"
+  echo "\t.screenrc"
   ln -sf ~/.termieter/screenrc ~/.screenrc
 
-  echo -e "\t.irbrc"
+  echo "\t.irbrc"
   ln -sf ~/.termieter/irbrc ~/.irbrc
