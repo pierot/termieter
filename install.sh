3977da8b84c69600f4008d40339f104646d0a9aa
Pieter Michels <pieter@noort.be>
2011-09-02 16:13:17 +0200

    removing config ir first

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ git clone git://github.com/pierot/termieter.git ~/.termieter
 
 echo "Linking"
 
-  echo -E "\t.bash_profile"
+  echo -e "\t.bash_profile"
   ln -sf ~/.termieter/bash_profile ~/.bash_profile
 
-  echo -E "\t.gitconfig"
+  echo -e "\t.gitconfig"
   ln -sf ~/.termieter/gitconfig ~/.gitconfig
 
-  echo -E "\t.screenrc"
+  echo -e "\t.screenrc"
   ln -sf ~/.termieter/screenrc ~/.screenrc
 
-  echo -E "\t.irbrc"
+  echo -e "\t.irbrc"
   ln -sf ~/.termieter/irbrc ~/.irbrc
