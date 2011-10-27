e7b8ed779dea4b07a24b5095c7dbd8bb59200063
Pieter Michels <pieterm@a00708.local>
2011-10-27 10:38:54 +0200

    edits

--- install.sh
+++ install.sh
@@ -46,90 +46,86 @@ done
 
 ############################################################################### 
 
-_print "Installing termieter files ***********************"
-
-  cd ~
+cd ~
 
 _print "Removing current termieter installation"
 
-  rm -rf "$install_dir"
+rm -rf "$install_dir"
 
-_print "Check if 'git' exists"
+GIT_INSTALLED=true
 
-  GIT_INSTALLED=true
+hash git 2>&- || { _error "I require git but it's not installed!"; GIT_INSTALLED=false; }
 
-  hash git 2>&- || { _error "I require git but it's not installed!"; GIT_INSTALLED=false; }
+if ! $GIT_INSTALLED; then
+  while true
+  do
+    read -p "Do you want me to install git? (sudo needed) [Y/N] " RESP
 
-  if ! $GIT_INSTALLED; then
-    while true
-    do
-      read -p "Do you want me to install git? (sudo needed) [Y/N] " RESP
+    case $RESP
+      in
+      [yY])
+        _check_root
+        _system_installs_install 'git'
 
-      case $RESP
-        in
-        [yY])
-          _check_root
-          _system_installs_install 'git'
+        GIT_INSTALLED=true
+        break
+        ;;
+      [nN])
+        break
+        ;;
+      *)
+        echo "Please enter Y or N"
+    esac
+  done
+fi
 
-          GIT_INSTALLED=true
-          break
-          ;;
-        [nN])
-          break
-          ;;
-        *)
-          echo "Please enter Y or N"
-      esac
-    done
-  fi
+if $GIT_INSTALLED; then
+  git clone git://github.com/pierot/termieter.git "$install_dir"
+fi
 
-_print "Cloning into repo"
+if [ ! -d "$install_dir" ]; then
+  _error "Termieter doesn't seem to be installed correctly. Aborting"
 
-  if $GIT_INSTALLED; then
-    git clone git://github.com/pierot/termieter.git "$install_dir"
-  fi
+  exit 1
+else
+  mkdir -p ~/.bash_backup
 
-  if [ ! -d "$install_dir" ]; then
-    _error "Termieter doesn't seem to be installed correctly. Aborting"
+  _back_install() {
+    if [ ! -n "$1" ]; then
+      _error "Error in install script. Aborting"
 
-    exit 1
-  else
-    _print "Backup all previous files"
-      
-      mkdir -p ~/.bash_backup
+      exit 1
+    fi
 
-      _back_install() {
-        if [ ! -n "$1" ]; then
-          _error "Error in install script. Aborting"
+    if [ -f "$HOME/.$1" ]; then
+      mv "$HOME/.$1" "$HOME/.bash_backup/.$1" 
+    fi
 
-          exit 1
-        fi
+    if [ -f "$install_dir/symlinks/$1" ]; then
+      if [ -n "$2" ]; then
+        ln -sf "$install_dir/symlinks/$1" "$HOME/.$2"
+      else
+        ln -sf "$install_dir/symlinks/$1" "$HOME/.$1"
+      fi
+    fi
+  }
 
-        if [ -f "$HOME/.$1" ]; then
-          mv "$HOME/.$1" "$HOME/.bash_backup/.$1" 
-        fi
+  for sym_file in "$install_dir/symlinks/*"
+  do
+    _back_install "$sym_file"
+    # _back_install "bash_profile"
+    # _back_install "profile"
+    # _back_install "bashrc"
+    # _back_install "gemrc"
+    # _back_install "gitconfig"
+    # _back_install "gitignore"
+    # _back_install "gvimrc.local"
+    # _back_install "irbrc"
+    # _back_install "screenrc"
+    # _back_install "vimrc.local"
+  done
 
-        if [ -f "$install_dir/symlinks/$1" ]; then
-          if [ -n "$2" ]; then
-            ln -sf "$install_dir/symlinks/$1" "$HOME/.$2"
-          else
-            ln -sf "$install_dir/symlinks/$1" "$HOME/.$1"
-          fi
-        fi
-      }
-
-      _back_install "bash_profile"
-      _back_install "profile"
-      _back_install "bashrc"
-      _back_install "gemrc"
-      _back_install "gitconfig"
-      _back_install "gitignore"
-      _back_install "gvimrc.local"
-      _back_install "irbrc"
-      _back_install "screenrc"
-      _back_install "vimrc.local"
-
-    _print "Installation finished **************************"
-  fi
+  _print "Installation finished"
+fi
 
 _cleanup_lib
