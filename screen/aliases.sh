a0bf90930aff7a18005ed93bfbb1d4e2a129c51e
Pieter Michels <pieter@noort.be>
2012-01-05 08:33:07 +0100

    edits based on iTerm

--- screen/aliases.sh
+++ screen/aliases.sh
@@ -1,8 +1,8 @@
-alias screen='export SCREENPWD=$(pwd); /usr/bin/screen -U'
-alias s='export SCREENPWD=$(pwd); /usr/bin/screen -U'
+alias screen='export SCREENPWD=$(pwd); /usr/bin/screen -U -T $TERM'
+alias s='export SCREENPWD=$(pwd); /usr/bin/screen -U -T $TERM'
 
 case "$TERM" in 
-    'screen')
-         cd $SCREENPWD
-         ;; 
+  'screen')
+     cd $SCREENPWD
+     ;; 
 esac
