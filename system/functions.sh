fa0963e9ffb67fee5313edd51fd5978c710f2718
Pieter Michels <pieter@noort.be>
2012-01-17 15:34:12 +0100

    add currency converter

--- system/functions.sh
+++ system/functions.sh
@@ -9,6 +9,18 @@ function open-sesame () {
   cat ~/.ssh/id_rsa.pub | ssh $@ "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys";
 }
 
+# convert currency from command line
+function curr () {
+  if [ $1 == 'all' ]; then
+    wget -qO- "http://hints.macworld.com/dlfiles/curr_units.txt"; echo ""
+  else
+    for i in $3
+    do
+      wget -qO- "http://www.google.com/finance/converter?a=$1&from=$2&to=$i&hl=en" |  sed '/res/!d;s/<[^>]*>//g';
+    done
+  fi
+}
+
 # Helper :)
 function listf () {
   cat "$HOME/.termieter/system/functions.sh"
