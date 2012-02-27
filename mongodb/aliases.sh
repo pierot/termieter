c49951a311e087c1fd9eaf732e3628da8a608bc7
Pieter Michels <pieter@noort.be>
2012-02-27 16:16:40 +0100

    edits

--- mongodb/aliases.sh
+++ mongodb/aliases.sh
@@ -1,2 +1,2 @@
 alias mongo-backup='mongodump --host=127.0.0.1:27017 --out=~/.backups/backups/mongodb/dag-`date +%u`'
-alias mongo-start='mongod run --config /usr/local/Cellar/mongodb/1.8.3-x86_64/mongod.conf --rest'
+alias mongo-start='mongod run --config /usr/local/Cellar/mongodb/2.0.2-x86_64/mongod.conf --rest'
--- mysql/aliases.sh
+++ mysql/aliases.sh
@@ -1,3 +1,3 @@
-alias mysql-start='launchctl load /usr/local/Cellar/mysql/5.5.14/com.mysql.mysqld.plist'
-alias mysql-stop='launchctl unload /usr/local/Cellar/mysql/5.5.14/com.mysql.mysqld.plist'
+alias mysql-start='launchctl load /usr/local/Cellar/mysql/5.5.19/com.mysql.mysqld.plist'
+alias mysql-stop='launchctl unload /usr/local/Cellar/mysql/5.5.19/com.mysql.mysqld.plist'
 alias mysql-restart='mysql-stop | mysql-start'
