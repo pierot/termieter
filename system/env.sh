2d5dd2c62b11b1cde8b309ad2ba6ce36dc8880a0
Pieter Michels <pieter@noort.be>
2012-09-19 11:10:31 -0400

    no locale for linux

--- system/env.sh
+++ system/env.sh
@@ -25,11 +25,13 @@ export PATH
 
 export CLICOLOR=1;
 
-export LANG="it_IT.UTF-8"
-export LC_COLLATE="it_IT.UTF-8"
-export LC_CTYPE="it_IT.UTF-8"
-export LC_MESSAGES="it_IT.UTF-8"
-export LC_MONETARY="it_IT.UTF-8"
-export LC_NUMERIC="it_IT.UTF-8"
-export LC_TIME="it_IT.UTF-8"
-export LC_ALL=
+if [[ `uname` == 'Darwin' ]]; then
+  export LANG="it_IT.UTF-8"
+  export LC_COLLATE="it_IT.UTF-8"
+  export LC_CTYPE="it_IT.UTF-8"
+  export LC_MESSAGES="it_IT.UTF-8"
+  export LC_MONETARY="it_IT.UTF-8"
+  export LC_NUMERIC="it_IT.UTF-8"
+  export LC_TIME="it_IT.UTF-8"
+  export LC_ALL=
+fi
