ec5882955b1a3f4f3814f98d9c8d8dc362ff0c80
Pieter Michels <pieter@noort.be>
2012-09-19 11:16:02 -0400

    better rbenv / rvm support

---
+++ ruby/rbenv.sh
@@ -0,0 +1,5 @@
+if [ -s "$HOME/.rbenv/bin" ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+
+  eval "$(rbenv init -)"
+fi
--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -1,5 +1,8 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+  . "$HOME/.rvm/scripts/rvm"
 
-if groups | grep -q rvm; then
-  source '/usr/local/lib/rvm'
+  if groups | grep -q rvm; then
+    source '/usr/local/lib/rvm'
+  fi
 fi
+
