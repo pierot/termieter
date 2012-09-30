f39c805287d05f179b52b5aaa0df76317feb8eb3
Pieter Michels <pieter@noort.be>
2012-09-30 20:55:48 +0200

    edit

--- ruby/aliases.sh
+++ ruby/aliases.sh
@@ -2,9 +2,9 @@ alias be='echo "» bundle exec"; bundle exec'
 alias b='echo "» bundle"; bundle'
 alias r='echo "» be rails"; be rails'
 
-# alias rt='echo "» be rails s thin"; be rails s thin'
+alias rt='echo "» be rails s thin"; be rails s thin'
 
 alias f='echo "» be foreman start -p 3000"; be foreman start -p 3000'
 
-alias c='echo "» cap deploy"; cap deploy'
-alias cm='echo "» cap deploy:migrations"; cap deploy:migrations'
+alias c='echo "» be cap deploy"; be cap deploy'
+alias cm='echo "» be cap deploy:migrations"; be cap deploy:migrations'
