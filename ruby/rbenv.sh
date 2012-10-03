28cca1e03a01a83b937d8ba1ffb8847199254908
Pieter Michels <pieter@noort.be>
2012-10-03 22:58:44 +0200

    rbenv issue

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -1,9 +1,13 @@
-if [ -s "$HOME/.rbenv/bin" ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-
-  eval "$(rbenv init -)"
-elif [ -s "/usr/local/rbenv/bin" ]; then
-  export PATH="/usr/local/rbenv/bin:$PATH"
-
+if which rbenv &> /dev/null; then
   eval "$(rbenv init -)"
 fi
+
+# if [ -s "$HOME/.rbenv/bin" ]; then
+#   export PATH="$HOME/.rbenv/bin:$PATH"
+# 
+#   eval "$(rbenv init -)"
+# elif [ -s "/usr/local/rbenv/bin" ]; then
+#   export PATH="/usr/local/rbenv/bin:$PATH"
+# 
+#   eval "$(rbenv init -)"
+# fi
