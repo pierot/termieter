c303df6f11e598765bac045b112222241adef5e8
Pieter Michels <pieter@noort.be>
2012-10-23 15:24:59 +0200

    fix

--- system/functions.sh
+++ system/functions.sh
@@ -1,42 +1,51 @@
-# Create a new directory and enter it
-md() {
-  mkdir -p "$@" && cd "$@"
-}
+###############################################################################
+# SSH
 
-# Copy your public ssh key to remote server for password-less login
-# Usage: ssh-sesame 'tortuga'
-function ssh-sesame() {
-  cat "$HOME/.ssh/id_rsa.pub" | ssh $@ "mkdir -p $HOME/.ssh && cat >> $HOME/.ssh/authorized_keys";
-}
+  # Copy your public ssh key to remote server for password-less login
+  # Usage: ssh-sesame 'tortuga'
+  function ssh-sesame() {
+    cat "$HOME/.ssh/id_rsa.pub" | ssh $@ "mkdir -p $HOME/.ssh && cat >> $HOME/.ssh/authorized_keys";
+  }
 
-# Copy public ssh key
-function ssh-public() {
-  echo "» cat $HOME/.ssh/id_rsa.pub | pbcopy"
+  # Print public ssh key
+  function ssh-public() {
+    echo "» cat $HOME/.ssh/id_rsa.pub"
 
-  cat "$HOME/.ssh/id_rsa.pub" | pbcopy
-}
+    cat "$HOME/.ssh/id_rsa.pub"
+  }
 
-# Clear temp files for a faster Terminal
-function fasterfaster() {
-  sudo rm -rf /private/var/log/asl/*
-  sudo rm -rf /var/mail/*
-}
+###############################################################################
+# SYSTEM
 
-# Remove spaces and replace by a dash
-function remove-spaces() {
-  for file in *
-  do 
-    mv "$file" "${file// /-}"
-  done
-}
+  # Clear temp files for a faster Terminal
+  function fasterfaster() {
+    sudo rm -rf /private/var/log/asl/*
+    sudo rm -rf /var/mail/*
+  }
 
-# Remove underscore and replace by a dash
-function remove-underscore() {
-  for file in *
-  do 
-    mv "$file" "${file//_/-}"
-  done
-}
+  # Create a new directory and enter it
+  md() {
+    mkdir -p "$@" && cd "$@"
+  }
+
+###############################################################################
+# RENAMING
+
+  # Remove spaces and replace by a dash
+  function remove-spaces() {
+    for file in *
+    do 
+      mv "$file" "${file// /-}"
+    done
+  }
+
+  # Remove underscore and replace by a dash
+  function remove-underscore() {
+    for file in *
+    do 
+      mv "$file" "${file//_/-}"
+    done
+  }
 
 # List all files interacting with ipv4-6 ports
 function list-files-ports() {
@@ -45,46 +54,61 @@ function list-files-ports() {
   lsof -i
 }
 
-function disk-space() {
-  echo "» df -h"
+###############################################################################
+# USAGE
 
-  df -h
-}
+  function usage-disk() {
+    echo "» df -h"
 
-function dir-space() {
-  echo "» du -ch | grep total"
+    df -h
+  }
 
-  du -ch | grep total
-}
+  function usage-dir() {
+    echo "» du -ch | grep total"
 
+    du -ch | grep total
+  }
+
+###############################################################################
 # Helper :)
-function listf() {
-  cat "$TRM/system/functions.sh"
-}
 
-if [[ `uname` == 'Darwin' ]]; then
-
-  # OSX
-
-  # Fix font caches
-  function fix-fonts() {
-    echo "» atsutil databases -removeUser; atsutil server -shutdown; atsutil server -ping"
-
-    atsutil databases -removeUser
-    atsutil server -shutdown
-    atsutil server -ping
+  function listf() {
+    cat "$TRM/system/functions.sh"
   }
 
-else
-  
-  # LINUX
+###############################################################################
+# OS
 
-  function list-services() {
-    chkconfig --list | grep '3:on'
-  }
+  if [[ `uname` == 'Darwin' ]]; then
 
-  function active-connections() {
-    netstat -tulpn
-  }
+    # OSX
 
-fi
+    # Fix font caches
+    function fix-fonts() {
+      echo "» atsutil databases -removeUser; atsutil server -shutdown; atsutil server -ping"
+
+      atsutil databases -removeUser
+      atsutil server -shutdown
+      atsutil server -ping
+    }
+
+    # Copy public ssh key
+    function ssh-public-copy() {
+      echo "» cat $HOME/.ssh/id_rsa.pub | pbcopy"
+
+      cat "$HOME/.ssh/id_rsa.pub" | pbcopy
+    }
+
+  else
+    
+    # LINUX
+
+    function list-services() {
+      chkconfig --list | grep '3:on'
+    }
+
+    function active-connections() {
+      netstat -tulpn
+    }
+
+  fi
