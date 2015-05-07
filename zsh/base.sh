66889c2cf4575f8bba1a699c3b6dd0cf6cc6d9b0
Pieter Michels <pieter@noort.be>
2015-05-07 13:28:19 +0200

    solarized

--- zsh/base.sh
+++ zsh/base.sh
@@ -80,8 +80,8 @@ if [[ $OS == 'OSX' ]]; then
   # }
 
   # MYSQL
-  alias mysql-start='launchctl load /usr/local/Cellar/mysql/5.6.23/homebrew.mxcl.mysql.plist'
-  alias mysql-stop='launchctl unload /usr/local/Cellar/mysql/5.6.23/homebrew.mxcl.mysql.plist'
+  alias mysql-start='launchctl load /usr/local/Cellar/mysql/5.6.24/homebrew.mxcl.mysql.plist'
+  alias mysql-stop='launchctl unload /usr/local/Cellar/mysql/5.6.24/homebrew.mxcl.mysql.plist'
   alias mysql-restart='mysql-stop | mysql-start'
 
   # POSTGRESQL
