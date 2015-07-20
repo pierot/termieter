73de0cda0087cbfbbf7e5b55ba65c293019f9a47
Pieter Michels <pieter@noort.be>
2015-07-20 13:08:27 +0200

    term

--- zsh/base.sh
+++ zsh/base.sh
@@ -19,7 +19,9 @@ alias ls='ls $LS_OPT' # long list, excludes dot files
 alias ll='ls $LS_OPT -GlhA' # long list all, includes dot files
 alias l='ls $LS_OPT -1AFC'
 
-alias cat='ccat'
+if [[ $OS == 'OSX' ]]; then
+  alias cat='ccat'
+fi
 
 alias termieter="cd $TRM"
 alias termietere="cd $TRM; vim ."
