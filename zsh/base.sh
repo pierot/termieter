17c341cd6be28af33ac93f3a57973cfc4878d4e3
Pieter Michels <pieter@noort.be>
2017-10-23 15:58:30 +0200

    update

--- zsh/base.sh
+++ zsh/base.sh
@@ -231,8 +231,25 @@ export PATH="$PATH:/usr/local/mysql/bin"
 
 ##########################################################
 
-# FZF fuzzy finder
-export FZF_DEFAULT_COMMAND='ag -g ""'
+# fzf via Homebrew
+if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
+  source /usr/local/opt/fzf/shell/key-bindings.zsh
+  source /usr/local/opt/fzf/shell/completion.zsh
+fi
+
+# fzf via local installation
+if [ -e ~/.fzf ]; then
+  export PATH="$HOME/.fzf/bin:$PATH"
+  source ~/.fzf/shell/key-bindings.zsh
+  source ~/.fzf/shell/completion.zsh
+fi
+
+# fzf + ag configuration
+if have fzf && have ag; then
+  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
 
 ##########################################################
 
