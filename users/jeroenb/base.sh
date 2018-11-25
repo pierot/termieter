e325c2f6b002de5db32ed0efe5c4cc6b0536412d
Jeroen Bourgois <jeroenbourgois@gmail.com>
2018-11-25 18:08:02 +0100

    cleanup

--- users/jeroenb/base.sh
+++ users/jeroenb/base.sh
@@ -1,19 +1,25 @@
 ###################################
 # NeoVIM switch
 
-export EDITOR=nvim
+have() {
+  type "$1" &> /dev/null
+}
 
-alias v="nvim ."
-alias vim="nvim"
-alias vi="nvim"
-alias vimdiff='nvim -d'
-alias oldvim="/usr/local/bin/vim"
+if have nvim; then
+  export EDITOR=nvim
 
-###################################
+  alias v="nvim ."
+  alias vim="nvim"
+  alias vi="nvim"
+  alias vimdiff='nvim -d'
+  alias oldvim="/usr/local/bin/vim"
+fi
 
 # neomutt
-alias mutt="neomutt"
-alias m="neomutt"
+if have neomutt; then
+  alias mutt="neomutt"
+  alias m="neomutt"
+fi
 
 ###################################
 
@@ -22,8 +28,7 @@ H=$(date +%H)
 if [[ $OS == 'OSX' ]]; then
   if [[ $H == 8 ]]; then
     # Keyrepeat
-    defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-    defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+    defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
   fi
 fi
 
