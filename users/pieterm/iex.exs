64b7440a9436385b0fae8180b1511ee6fd0338a8
Pieter Michels <pieter@noort.be>
2020-01-15 10:25:00 +0100

    tweaking

--- users/pieterm/iex.exs
+++ users/pieterm/iex.exs
@@ -2,31 +2,73 @@
 # IEx.configure colors: [ eval_result: [ :cyan, :bright ] ]
 # IO.puts(IO.ANSI.red_background() <> IO.ANSI.white() <> " ❄❄ go go Elixir ❄❄ " <> IO.ANSI.reset())
 
-Application.put_env(:elixir, :ansi_enabled, true)
+# Application.put_env(:elixir, :ansi_enabled, true)
+#
+# IEx.configure(
+#   colors: [
+#     eval_result: [:green, :bright],
+#     eval_error: [[:red, :bright, "Bug Bug ..!!"]],
+#     eval_info: [:yellow, :bright]
+#   ],
+#   default_prompt:
+#     [
+#       # ANSI CHA, move cursor to column 1
+#       "\e[G",
+#       :green,
+#       "%prefix",
+#       :white,
+#       "|",
+#       :blue,
+#       "%counter",
+#       :white,
+#       "|",
+#       :red,
+#       # plain string
+#       "▶",
+#       :reset
+#     ]
+#     |> IO.ANSI.format()
+#     |> IO.chardata_to_string()
+# )
 
-IEx.configure(
-  colors: [
-    eval_result: [:green, :bright],
-    eval_error: [[:red, :bright, "Bug Bug ..!!"]],
-    eval_info: [:yellow, :bright]
-  ],
-  default_prompt:
-    [
-      # ANSI CHA, move cursor to column 1
-      "\e[G",
-      :green,
-      "%prefix",
-      :white,
-      "|",
-      :blue,
-      "%counter",
-      :white,
-      "|",
-      :red,
-      # plain string
-      "▶",
-      :reset
-    ]
-    |> IO.ANSI.format()
-    |> IO.chardata_to_string()
-)
+import_if_available(Ecto.Query)
+import_if_available(Ecto.Changeset)
+
+defmodule AC do
+  def update(schema, changes) do
+    schema
+    |> Ecto.Changeset.change(changes)
+    |> Repo.update()
+  end
+
+  IEx.configure(
+    colors: [
+      syntax_colors: [
+        number: :light_yellow,
+        atom: :light_cyan,
+        string: :light_black,
+        boolean: [:light_blue],
+        nil: [:magenta, :bright]
+      ],
+      ls_directory: :cyan,
+      ls_device: :yellow,
+      doc_code: :green,
+      doc_inline_code: :magenta,
+      doc_headings: [:cyan, :underline],
+      doc_title: [:cyan, :bright, :underline]
+    ],
+    default_prompt:
+      [
+        # ANSI CHA, move cursor to column 1
+        "\e[G",
+        :light_magenta,
+        # plain string
+        "🧪 iex",
+        ">",
+        :white,
+        :reset
+      ]
+      |> IO.ANSI.format()
+      |> IO.chardata_to_string()
+  )
+end
