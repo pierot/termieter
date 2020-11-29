ad79172b6cbeb26ef87918a89ddbc2416188eb96
Jeroen Bourgois <jeroenbourgois@gmail.com>
2020-11-29 10:48:21 +0100

    feat: alias mv for safety

--- zsh/base.sh
+++ zsh/base.sh
@@ -83,9 +83,10 @@ fi
 
 ##########################################################
 
-alias ls='ls $LS_OPT' # long list, excludes dot files
+alias ls='ls $LS_OPT'       # long list, excludes dot files
 alias ll='ls $LS_OPT -GlhA' # long list all, includes dot files
 alias l='ls $LS_OPT -1AFC'
+alias mv='mv -i'            # prevents accidental overwrite
 
 alias termieter="cd $TRM"
 alias termietere="cd $TRM; vim ."
