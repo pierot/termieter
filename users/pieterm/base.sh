b3e9ed3948009844aa7edcc84edf580a0b0c8f61
Pieter Michels <pieter@noort.be>
2021-05-27 22:44:22 +0200

    lua tests

--- users/pieterm/base.sh
+++ users/pieterm/base.sh
@@ -16,11 +16,11 @@ fi
 
 export ICLOUD="~/Library/Mobile\ Documents/com~apple~CloudDocs"
 
-if [ -d "~/Library/Mobile Documents/com~apple~CloudDocs" ]; then
+# if [ -d "~/Library/Mobile\ Documents/com\~apple\~CloudDocs" ]; then
   alias icloud="cd $ICLOUD"
   alias repos="cd $ICLOUD/Work/repos/"
   alias dev="cd $ICLOUD/Work/devel/"
-fi
+# fi
 
 export DROPBOX="~/Dropbox"
 
