bb0b299eaaf748fa4ba59f760fb45b122c90e839
Pieter Michels <pieter@noort.be>
2021-06-24 21:31:32 +0200

    test

--- users/pieterm/config/nvim/lua/init.lua
+++ users/pieterm/config/nvim/lua/init.lua
@@ -205,16 +205,17 @@ map('n', 'R', '<cmd>NvimTreeRefresh<CR>')
 require('lualine').setup()
 
 -- Bufferline
-require('bufferline').setup {
-  options = {
-    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
-    diagnostics = "nvim_lsp",
-  }
-}
+-- require('bufferline').setup {
+--   options = {
+--     offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
+--     diagnostics = "nvim_lsp",
+--   }
+-- }
 
 -- Telescope
 -- Check to extend: https://github.com/varbhat/dotfiles/blob/main/dot_config/nvim/lua/utils/telescope.lua
 map('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
+map('n', '<c-p>', '<cmd>Telescope find_files<CR>')
 map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
 
 require('telescope').setup {
@@ -268,47 +269,49 @@ ts.setup({
     "lua", "query", "elixir", "dockerfile", "php", 
   },
   highlight = {
-    enable = true
+    enable = {
+      enabled = true, 
+      use_languagetree = true
+    }
   },
-  -- highlight = {enable = {enabled = true, use_languagetree = true}},
   indent = {
     enable = true
   },
   textobjects = { 
     enable = true 
   },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
-  },
-  refactor = {
-    highlight_definitions = {enable = true},
-    -- highlight_current_scope = {enable = true},
-    smart_rename = {
-      enable = true,
-      keymaps = {smart_rename = "grr"},
-      navigation = {
-        enable = true,
-        keymaps = {
-          goto_definition = "gnd",
-          list_definitions = "gnD",
-          list_definitions_toc = "gO",
-          goto_next_usage = "<a-*>",
-          goto_previous_usage = "<a-#>"
-        }
-      }
-    }
-  },
-  autotag = {enable = true}
+  -- rainbow = {
+  --   enable = true,
+  --   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+  --   max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
+  -- },
+  -- refactor = {
+  --   highlight_definitions = {enable = true},
+  --   -- highlight_current_scope = {enable = true},
+  --   smart_rename = {
+  --     enable = true,
+  --     keymaps = {smart_rename = "grr"},
+  --     navigation = {
+  --       enable = true,
+  --       keymaps = {
+  --         goto_definition = "gnd",
+  --         list_definitions = "gnD",
+  --         list_definitions_toc = "gO",
+  --         goto_next_usage = "<a-*>",
+  --         goto_previous_usage = "<a-#>"
+  --       }
+  --     }
+  --   }
+  -- },
+  -- autotag = {enable = true}
 })
 
 -- LSP
-require('lspconfig').elixirls.setup({
-  cmd = { "/usr/local/share/elixir-ls/language_server.sh" },
-})
+-- require('lspconfig').elixirls.setup({
+--   cmd = { "/usr/local/share/elixir-ls/language_server.sh" },
+-- })
 
-require('lspconfig').tsserver.setup({})
+-- require('lspconfig').tsserver.setup({})
 
 -- local lspfuzzy = require 'lspfuzzy'
 --
