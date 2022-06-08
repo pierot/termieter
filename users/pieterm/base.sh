f865caacb793a8d27a2a2e01ad2aa5364842aafd
Pieter Michels <pieter@noort.be>
2022-06-08 10:10:05 +0200

    cleanup

--- users/pieterm/base.sh
+++ users/pieterm/base.sh
@@ -16,48 +16,52 @@ fi
 alias ap='ansible-playbook'
 
 # Login via root and no password
-dockr-mysql() {
-  docker rm mysql
-  docker run \
-    -d \
-    -v "/Users/pieterm/Work/mysql/data":/var/lib/mysql \
-    -e MYSQL_ROOT_PASSWORD= \
-    -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
-    -p 3306:3306 \
-    --name mysql \
-    --platform linux/x86_64/v8 \
-    mysql:8.0.27
-}
-
-podman-mysql() {
-  # --security-opt label=disable \
-  # -u root \
-  # --privileged \
-    #--userns keep-id \
-  podman rm mysql
-  podman run \
-    --log-level=debug \
-    -d \
-    --rm \
-    -v "/Users/pieterm/Work/mysql/data":/var/lib/mysql \
-    -e MYSQL_ROOT_PASSWORD= \
-    -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
-    -p 3306:3306 \
-    --name mysql \
-    --platform linux/arm64 \
-    mysql:8.0.28-oracle
-}
-
-dockr-postgresql() {
-  docker rm postgresql
-  docker run \
-    -d \
-    -v "/Users/pieterm/Work/postgresql/data":/var/lib/postgresql/data \
-    -e POSTGRES_PASSWORD= \
-    -e POSTGRES_USER=jackjoe \
-    -e POSTGRES_HOST_AUTH_METHOD=trust \
-    -p 5432:5432 \
-    --name postgresql \
-    --platform linux/x86_64/v8 \
-    postgres:14
-}
+# dockr-mysql() {
+#     # -v "/opt/homebrew/var/mysql":/var/lib/mysql \
+#     # -v mysql:/var/lib/mysql \
+#     # --platform linux/x86_64/v8 \
+#   docker rm mysql
+#   docker volume create --name mysql
+#   docker run \
+#     -d \
+#     -v mysql:/var/lib/mysql \
+#     -e MYSQL_ROOT_PASSWORD= \
+#     -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+#     -p 3306:3306 \
+#     --name mysql \
+#     --platform linux/arm64 \
+#     mysql:8.0.29-oracle
+# }
+#
+# podman-mysql() {
+#   # --security-opt label=disable \
+#   # -u root \
+#   # --privileged \
+#     #--userns keep-id \
+#   podman rm mysql
+#   podman run \
+#     --log-level=debug \
+#     -d \
+#     --rm \
+#     -v "/opt/homebrew/var/mysql":/var/lib/mysql \
+#     -e MYSQL_ROOT_PASSWORD= \
+#     -e MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+#     -p 3306:3306 \
+#     --name mysql \
+#     --platform linux/arm64 \
+#     mysql:8.0.29-oracle
+# }
+#
+# dockr-postgresql() {
+#   docker rm postgresql
+#   docker run \
+#     -d \
+#     -v "/Users/pieterm/Work/postgresql/data":/var/lib/postgresql/data \
+#     -e POSTGRES_PASSWORD= \
+#     -e POSTGRES_USER=jackjoe \
+#     -e POSTGRES_HOST_AUTH_METHOD=trust \
+#     -p 5432:5432 \
+#     --name postgresql \
+#     --platform linux/x86_64/v8 \
+#     postgres:14
+# }
--- zsh/base.sh
+++ zsh/base.sh
@@ -24,6 +24,19 @@ if [[ $OS == 'OSX' ]]; then
     sudo ifconfig en0 up
     echo "Your new physical address is $mac"
   }
+
+  tweakMac() {
+    # opening and closing Quick Look windows
+    defaults write -g QLPanelAnimationDuration -float 0
+
+    # sending messages and opening windows for replies
+    defaults write com.apple.Mail DisableSendAnimations -bool true
+    defaults write com.apple.Mail DisableReplyAnimations -bool true
+
+    # Keyrepeat
+    defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
+    defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+  }
 else
   list-services() {
     chkconfig --list | grep '3:on'
@@ -38,53 +51,6 @@ else
     sudo rm /var/cache/apt/archives/lock
     sudo rm /var/lib/dpkg/lock
   }
-fi
-
-##########################################################
-
-# OSX Defaults
-if [[ $OS == 'OSX' ]]; then
-  H=$(date +%H)
-
-  if [[ $H == 8 ]]; then
-    # opening and closing windows and popovers
-    defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
-    # smooth scrolling
-    defaults write -g NSScrollAnimationEnabled -bool false
-    # showing and hiding sheets, resizing preference windows, zooming windows
-    # float 0 doesn't work
-    defaults write -g NSWindowResizeTime -float 0.001
-    # opening and closing Quick Look windows
-    defaults write -g QLPanelAnimationDuration -float 0
-    # rubberband scrolling (doesn't affect web views)
-    defaults write -g NSScrollViewRubberbanding -bool false
-    # resizing windows before and after showing the version browser
-    # also disabled by NSWindowResizeTime -float 0.001
-    defaults write -g NSDocumentRevisionsWindowTransformAnimation -bool false
-    # showing a toolbar or menu bar in full screen
-    defaults write -g NSToolbarFullScreenAnimationDuration -float 0
-    # scrolling column views
-    defaults write -g NSBrowserColumnAnimationSpeedMultiplier -float 0
-    # showing the Dock
-    defaults write com.apple.dock autohide-time-modifier -float 0
-    defaults write com.apple.dock autohide-delay -float 0
-    # showing and hiding Mission Control, command+numbers
-    defaults write com.apple.dock expose-animation-duration -float 0
-    # showing and hiding Launchpad
-    defaults write com.apple.dock springboard-show-duration -float 0
-    defaults write com.apple.dock springboard-hide-duration -float 0
-    # changing pages in Launchpad
-    defaults write com.apple.dock springboard-page-duration -float 0
-    # at least AnimateInfoPanes
-    defaults write com.apple.finder DisableAllAnimations -bool true
-    # sending messages and opening windows for replies
-    defaults write com.apple.Mail DisableSendAnimations -bool true
-    defaults write com.apple.Mail DisableReplyAnimations -bool true
-
-    # Keyrepeat
-    defaults write -g InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
-    defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-  fi
 fi
 
 ##########################################################
