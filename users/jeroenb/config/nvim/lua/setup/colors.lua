2c37e43d8afee8a12709f0404b6072f5b3b72abc
Jeroen Bourgois <jeroen@jackjoe.be>
2022-12-02 10:21:23 +0100

    chore: cleanup

--- users/jeroenb/config/nvim/lua/setup/colors.lua
+++ users/jeroenb/config/nvim/lua/setup/colors.lua
@@ -1,11 +1,11 @@
 local u = require('utils')
 
 --vim.cmd("colorscheme jellybeans-nvim")
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme tokyonight-night")
 
-u.map('n', '<silent> [oh', ':call gruvbox#hls_show()<CR>')
-u.map('n', '<silent> ]oh', ':call gruvbox#hls_hide()<CR>')
-u.map('n', '<silent> coh', ':call gruvbox#hls_toggle()<CR>')
+-- u.map('n', '<silent> [oh', ':call gruvbox#hls_show()<CR>')
+-- u.map('n', '<silent> ]oh', ':call gruvbox#hls_hide()<CR>')
+-- u.map('n', '<silent> coh', ':call gruvbox#hls_toggle()<CR>')
 
 function ToggleTheme()
   if vim.o.background == "dark" then
@@ -17,8 +17,8 @@ end
 
 u.map('n', '<F5>', ':lua ToggleTheme()<CR>')           -- Switch between windows by hitting <Tab> twice
 
-vim.api.nvim_exec([[
-nnoremap * :let @/ = ""<CR>:call gruvbox#hls_show()<CR>*
-nnoremap / :let @/ = ""<CR>:call gruvbox#hls_show()<CR>/
-nnoremap ? :let @/ = ""<CR>:call gruvbox#hls_show()<CR>?
-]], true)
+-- vim.api.nvim_exec([[
+-- nnoremap * :let @/ = ""<CR>:call gruvbox#hls_show()<CR>*
+-- nnoremap / :let @/ = ""<CR>:call gruvbox#hls_show()<CR>/
+-- nnoremap ? :let @/ = ""<CR>:call gruvbox#hls_show()<CR>?
+-- ]], true)
