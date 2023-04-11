81fdf460b138ac18eba05d62fdd72a46ec09a821
Pieter Michels <pieter@noort.be>
2023-04-11 11:19:33 +0200

    chore: cleanup

--- users/pieterm/config/nvim/lua/setup/lspconfig.lua
+++ users/pieterm/config/nvim/lua/setup/lspconfig.lua
@@ -1,27 +1,217 @@
-local status, nvim_lsp = pcall(require, "lspconfig")
-if (not status) then return end
+-- local status, nvim_lsp = pcall(require, "lspconfig")
+-- if (not status) then return end
+-- 
+-- local protocol = require('vim.lsp.protocol')
+-- 
+-- -- Use an on_attach function to only map the following keys
+-- -- after the language server attaches to the current buffer
+-- local on_attach = function(client, bufnr)
+--   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+--   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+-- 
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+-- 
+--   -- Mappings.
+--   local opts = { noremap = true, silent = true }
+-- 
+--   -- See `:help vim.lsp.*` for documentation on any of the below functions
+--   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+--   --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--   --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+-- end
+-- 
+-- protocol.CompletionItemKind = {
+--   '', -- Text
+--   '', -- Method
+--   '', -- Function
+--   '', -- Constructor
+--   '', -- Field
+--   '', -- Variable
+--   '', -- Class
+--   'ﰮ', -- Interface
+--   '', -- Module
+--   '', -- Property
+--   '', -- Unit
+--   '', -- Value
+--   '', -- Enum
+--   '', -- Keyword
+--   '﬌', -- Snippet
+--   '', -- Color
+--   '', -- File
+--   '', -- Reference
+--   '', -- Folder
+--   '', -- EnumMember
+--   '', -- Constant
+--   '', -- Struct
+--   '', -- Event
+--   'ﬦ', -- Operator
+--   '', -- TypeParameter
+-- }
+-- 
+-- -- Set up completion using nvim_cmp with LSP source
+-- local cmp_nvim_lsp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+-- local capabilities = {}
+-- if cmp_nvim_lsp_status_ok then
+--   -- return
+--   capabilities = cmp_nvim_lsp.default_capabilities()
+-- end
+-- 
+-- -- Elixir
+-- local path = require "mason-core.path"
+-- local path_to_elixirls = path.concat({vim.fn.stdpath "data", "mason", "bin", "elixir-ls"})
+-- 
+-- -- Setup
+-- --[[ nvim_lsp.elixirls.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   cmd = { path_to_elixirls },
+--   settings = {
+--     elixirLS = {
+--       fetchDeps = false,
+--       dialyzerFormat = "dialyxir_long",
+--       dialyzerEnabled = true,
+--       suggestSpecs = true
+--     }
+--   }
+-- }) ]]
+-- 
+-- nvim_lsp.tsserver.setup {
+--   on_attach = on_attach,
+--   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript" },
+--   cmd = { "typescript-language-server", "--stdio" },
+--   capabilities = capabilities
+-- }
+-- 
+-- nvim_lsp.tailwindcss.setup {}
+-- 
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--   underline = true,
+--   update_in_insert = false,
+--   virtual_text = { spacing = 4, prefix = "●" },
+--   severity_sort = true,
+-- })
+-- 
+-- -- Diagnostic symbols in the sign column (gutter)
+-- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+-- for type, icon in pairs(signs) do
+--   local hl = "DiagnosticSign" .. type
+--   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+-- end
+-- 
+-- vim.diagnostic.config({
+--   virtual_text = {
+--     prefix = '●'
+--   },
+--   update_in_insert = true,
+--   float = {
+--     source = "always", -- Or "if_many"
+--   },
+-- })
 
-local protocol = require('vim.lsp.protocol')
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+local lspconfig_setup, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_setup then
+  return
 end
 
+local cmp_nvim_lsp_setup, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_setup then
+  return
+end
+
+local typescript_setup, typescript = pcall(require, "typescript")
+if not typescript_setup then
+  return
+end
+
+local keymap = vim.keymap -- for conciseness
+
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  -- set keybinds
+  -- BRING BACK WHEN LSPSAGA WORKS...
+  -- keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+  -- keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  -- keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+  -- keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+  -- keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+  -- keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+  -- keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+  -- keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+  -- keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+  -- keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+  -- keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  -- keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+ -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+
+  -- typescript specific keymaps (e.g. rename file and update imports)
+  if client.name == "tsserver" then
+    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+  end
+end
+
+-- used to enable autocompletion (assign to every lsp server config)
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+-- configure html server
+lspconfig["html"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure typescript server with plugin
+typescript.setup({
+  server = {
+    capabilities = capabilities,
+    on_attach = on_attach,
+  },
+})
+
+-- configure css server
+lspconfig["cssls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure tailwindcss server
+lspconfig["tailwindcss"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure emmet language server
+lspconfig["emmet_ls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+})
+
+local protocol = require('vim.lsp.protocol')
 protocol.CompletionItemKind = {
   '', -- Text
   '', -- Method
@@ -50,21 +240,13 @@ protocol.CompletionItemKind = {
   '', -- TypeParameter
 }
 
--- Set up completion using nvim_cmp with LSP source
-local cmp_nvim_lsp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-local capabilities = {}
-if cmp_nvim_lsp_status_ok then
-  -- return
-  capabilities = cmp_nvim_lsp.default_capabilities()
-end
-
--- Elixir
+-- configure Elixir language server
 local path = require "mason-core.path"
-local path_to_elixirls = path.concat({vim.fn.stdpath "data", "mason", "bin", "elixir-ls"})
+local path_to_elixirls = path.concat({vim.fn.stdpath "data", "mason", "bin", "elixir-ls"}) 
 
 -- Setup
---[[ nvim_lsp.elixirls.setup({
-  on_attach = on_attach,
+lspconfig["elixirls"].setup({
+  on_attach = on_attach, 
   capabilities = capabilities,
   cmd = { path_to_elixirls },
   settings = {
@@ -75,22 +257,6 @@ local path_to_elixirls = path.concat({vim.fn.stdpath "data", "mason", "bin", "el
       suggestSpecs = true
     }
   }
-}) ]]
-
-nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript" },
-  cmd = { "typescript-language-server", "--stdio" },
-  capabilities = capabilities
-}
-
-nvim_lsp.tailwindcss.setup {}
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
 })
 
 -- Diagnostic symbols in the sign column (gutter)
