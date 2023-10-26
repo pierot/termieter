3f6de8f4ffbc86c971428cab46850737348f2a2e
Jeroen Bourgois <jeroen@jackjoe.be>
2023-10-26 15:30:13 +0200

    chore: tweak telescope

--- users/jeroenb/config/nvim/lua/jeroen/plugins/telescope.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/telescope.lua
@@ -1,52 +1,21 @@
---[[ local setup, telescope = pcall(require, "telescope")
-if (not setup) then return end
-
-local previewers_setup, previewers = pcall(require, "telescope.previewers")
-if (not previewers_setup) then return end
-
-local actions_setup, actions = pcall(require, "telescope.actions")
-if (not actions_setup) then return end
-
-local new_maker = function(filepath, bufnr, opts)
-  opts = opts or {}
-  if opts.use_ft_detect == nil then opts.use_ft_detect = true end
-  opts.use_ft_detect = false
-  previewers.buffer_previewer_maker(filepath, bufnr, opts)
-end
-
-telescope.setup {
-  defaults = {
-    buffer_previewer_maker = new_maker,
-    file_ignore_patterns = {"node_modules/.*", "vendor/.*"},
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
-    },
-    buffer_previewer_maker = new_maker,
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
-      }
-    }
-  },
-  pickers = {
-    find_files = {
-      theme = "ivy",
-      -- requires 'fd' te be installed
-      find_command = {'fd', '--type', 'f', '--hidden', '--exclude', '.git' }
-    },
-    buffers = {
-      theme = "ivy"
-    }
-  }
-} ]]
+-- if (not setup) then return end
+--
+-- local previewers_setup, previewers = pcall(require, "telescope.previewers")
+-- if (not previewers_setup) then return end
+--
+-- local actions_setup, actions = pcall(require, "telescope.actions")
+-- if (not actions_setup) then return end
+--
+-- local new_maker = function(filepath, bufnr, opts)
+--   opts = opts or {}
+--   if opts.use_ft_detect == nil then opts.use_ft_detect = true end
+--   opts.use_ft_detect = false
+--   previewers.buffer_previewer_maker(filepath, bufnr, opts)
+-- end
+--
+-- telescope.setup {
+--   defaults = {
+--     buffer_previewer_maker = new_maker,
 
 return {
 	"nvim-telescope/telescope.nvim",
@@ -63,7 +32,7 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "truncate " },
-				file_ignore_patterns = { "node_modules/.*", "vendor/.*" },
+				file_ignore_patterns = { "node_modules/.*", "vendor/.*", "*.min.js" },
 				vimgrep_arguments = {
 					"rg",
 					"--color=never",
@@ -81,6 +50,7 @@ return {
 					},
 				},
 			},
+			-- buffer_previewer_maker = new_maker,
 			pickers = {
 				find_files = {
 					theme = "ivy",
