15abd870b8b1bd8378ae901ba0c3606e9af97da0
Jeroen Bourgois <jeroen@jackjoe.be>
2023-10-26 15:44:40 +0200

    chore: trigger ack

--- users/jeroenb/config/nvim/lua/jeroen/plugins/ack.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/ack.lua
@@ -1,7 +1,7 @@
 return {
-	'mileszs/ack.vim',
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    vim.keymap.set('n', '<leader>a', ':Ack<space>')
-  end,
+	"mileszs/ack.vim",
+	event = "VeryLazy",
+	config = function()
+		vim.keymap.set("n", "<leader>a", ":Ack<space>")
+	end,
 }
