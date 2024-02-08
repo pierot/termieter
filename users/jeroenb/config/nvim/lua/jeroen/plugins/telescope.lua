750113eff7b03cf917328cca40a59eedef29a39e
Jeroen Bourgois <jeroen@jackjoe.be>
2024-02-08 13:53:35 +0100

    fix: telescope quicklist binding

--- users/jeroenb/config/nvim/lua/jeroen/plugins/telescope.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/telescope.lua
@@ -29,7 +29,7 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
 				},
 			},
