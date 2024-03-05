2ab438c474a1776f04c81d5daffa03b7f859bec4
Jeroen Bourgois <jeroen@jackjoe.be>
2024-03-05 09:16:32 +0100

    feat: colors

--- users/jeroenb/config/nvim/lua/jeroen/plugins/colors.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/colors.lua
@@ -10,12 +10,18 @@ return {
 	-- "rktjmp/lush.nvim",
 	"metalelf0/jellybeans-nvim",
 	"norcalli/nvim-colorizer.lua",
+	"Shatur/neovim-ayu",
+	"gruvbox-community/gruvbox",
 	{
-		"gruvbox-community/gruvbox",
+		"projekt0n/github-nvim-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			-- vim.cmd("colorscheme jellybeans-nvim")
-			vim.cmd("colorscheme lunaperche")
-			vim.keymap.set("n", "<F5>", ":lua ToggleTheme()<CR>") -- Switch between windows by hitting <Tab> twice
+			require("github-theme").setup({
+				-- ...
+			})
+
+			vim.cmd("colorscheme github_dark_high_contrast")
 		end,
 	},
 }
