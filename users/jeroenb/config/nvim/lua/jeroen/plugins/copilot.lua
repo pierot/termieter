4a613d258cdb780cf5311fda0068af06d87bdd1d
Jeroen Bourgois <jeroen@jackjoe.be>
2024-05-17 08:30:32 +0200

    chore: bring back copilot

--- users/jeroenb/config/nvim/lua/jeroen/plugins/copilot.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/copilot.lua
@@ -1,1 +1,10 @@
-return { "github/copilot.vim" }
+return {
+	"github/copilot.vim",
+	config = function()
+		local opts = { expr = true, replace_keycodes = false }
+		vim.keymap.set("i", "<C-C>", 'copilot#Accept("\\<CR>")', opts)
+		vim.keymap.set("i", "<C-O>", 'copilot#Accept("\\<CR>")', opts)
+		vim.keymap.set("i", "<C-T>", 'copilot#Accept("\\<CR>")', opts)
+		vim.g.copilot_no_tab_map = true
+	end,
+}
