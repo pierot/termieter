51de9a71f1637d0480c9dc54487a561cc7226521
Pieter Michels <pieter@noort.be>
2024-09-16 09:04:08 +0200

    tweak

--- users/pieterm/config/nvim/lua/setup/noice.lua
+++ users/pieterm/config/nvim/lua/setup/noice.lua
@@ -18,4 +18,19 @@ noice.setup({
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+  routes = {
+    {
+      filter = {
+        event = 'msg_show',
+        any = {
+          { find = '%d+L, %d+B' },
+          { find = '; after #%d+' },
+          { find = '; before #%d+' },
+          { find = '%d fewer lines' },
+          { find = '%d more lines' },
+        },
+      },
+      opts = { skip = true },
+    }
+  },
 })
--- users/pieterm/config/nvim/lua/setup/none-ls.lua
+++ users/pieterm/config/nvim/lua/setup/none-ls.lua
@@ -33,7 +33,8 @@ none_ls.setup({
         callback = function()
           -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
           -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-          vim.lsp.buf.formatting_sync()
+          -- vim.lsp.buf.formatting_sync()
+          vim.lsp.buf.format({ async = false })
         end,
       })
     end
