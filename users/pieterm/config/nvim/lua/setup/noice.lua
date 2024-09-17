5fcabf8b9ace8384cad3cc5496e948f46be07b3d
Pieter Michels <pieter@noort.be>
2024-09-17 07:01:56 +0200

    tweak

--- users/pieterm/config/nvim/lua/setup/noice.lua
+++ users/pieterm/config/nvim/lua/setup/noice.lua
@@ -18,4 +18,35 @@ noice.setup({
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+
+  views = {
+    cmdline_popup = {
+      position = {
+        row = "40%",
+        col = "50%",
+      },
+      --[[ size = {
+        width = 60,
+        height = "auto",
+      }, ]]
+    },
+    popupmenu = {
+      -- relative = "editor",
+      position = {
+        row = "40%",
+        col = "50%",
+      },
+      --[[ size = {
+        width = 60,
+        height = 10,
+      }, ]]
+      border = {
+        style = "rounded",
+        padding = { 3, 5 },
+      },
+      --[[ win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      }, ]]
+    },
+  },
 })
