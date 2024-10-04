2e9e16f28acfad0748163b0eb0652c88995f2059
Pieter Michels <pieter@noort.be>
2024-10-04 09:18:32 +0200

    shut up

--- users/pieterm/config/nvim/lua/setup/noice.lua
+++ users/pieterm/config/nvim/lua/setup/noice.lua
@@ -58,6 +58,7 @@ noice.setup({
           { find = '; before #%d+' },
           { find = '%d fewer lines' },
           { find = '%d more lines' },
+          { find = 'Phoenix.LiveView.HTMLFormatter' },
         },
       },
       opts = { skip = true },
