408b6a599037f71253e4feb66989ddb28b5bc72d
Pieter Michels <pieter@noort.be>
2024-10-04 15:39:40 +0200

    tweak

--- users/pieterm/iex.exs
+++ users/pieterm/iex.exs
@@ -1,6 +1,35 @@
 import_if_available(Ecto.Query)
 import_if_available(Ecto.Changeset)
 
+Application.put_env(:elixir, :ansi_enabled, true)
+
+IEx.configure(
+  colors: [
+    eval_result: [:green, :bright],
+    eval_error: [[:red, :bright, "Bug Bug ..!!"]],
+    eval_info: [:yellow, :bright]
+  ],
+  default_prompt:
+    [
+      # ANSI CHA, move cursor to column 1
+      "\e[G",
+      :green,
+      "%prefix",
+      :white,
+      "|",
+      :blue,
+      "%counter",
+      :white,
+      "|",
+      :red,
+      # plain string
+      "▶",
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
+)
+
 # defmodule AC do
 #   IEx.configure(
 #     colors: [
