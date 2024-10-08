7b566ba7660d8b3bfd60b24195a83958058cb484
Pieter Michels <pieter@noort.be>
2024-10-08 11:43:13 +0200

    fix

--- users/pieterm/iex.exs
+++ users/pieterm/iex.exs
@@ -1,6 +1,11 @@
 import_if_available(Ecto.Query)
 import_if_available(Ecto.Changeset)
 
+# IO.puts(
+#   IO.ANSI.magenta_background() <>
+#     IO.ANSI.white() <> " ❄❄❄ All things serve the BEAM ❄❄❄ " <> IO.ANSI.reset()
+# )
+
 Application.put_env(:elixir, :ansi_enabled, true)
 
 IEx.configure(
@@ -11,9 +16,6 @@ IEx.configure(
   ],
   default_prompt:
     [
-      # ANSI CHA, move cursor to column 1
-      "\e[G",
-      :green,
       "%prefix",
       :white,
       "|",
@@ -24,44 +26,12 @@ IEx.configure(
       :red,
       # plain string
       "▶",
+      :white,
+      # plain string
+      "▶▶",
+      # ❤ ❤-»" ,  # plain string
       :reset
     ]
     |> IO.ANSI.format()
     |> IO.chardata_to_string()
 )
-
-# defmodule AC do
-#   IEx.configure(
-#     colors: [
-#       syntax_colors: [
-#         number: :light_yellow,
-#         atom: :light_cyan,
-#         string: :light_black,
-#         boolean: [:light_blue],
-#         nil: [:magenta, :bright]
-#       ],
-#       eval_result: [:green, :bright],
-#       eval_error: [[:red, :bright, "Bug Bug ..!!"]],
-#       eval_info: [:yellow, :bright],
-#       ls_directory: :cyan,
-#       ls_device: :yellow,
-#       doc_code: :green,
-#       doc_inline_code: :magenta,
-#       doc_headings: [:cyan, :underline],
-#       doc_title: [:cyan, :bright, :underline]
-#     ],
-#     default_prompt:
-#     [
-#       # ANSI CHA, move cursor to column 1
-#       "\e[G",
-#       :light_magenta,
-#       # plain string
-#       "iex",
-#       ">",
-#       :white,
-#       :reset
-#     ]
-#     |> IO.ANSI.format()
-#     |> IO.chardata_to_string()
-#   )
-# end
