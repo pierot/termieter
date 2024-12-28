7e26d4b2874330b67d47d47f207ba687a386c1a2
Jeroen Bourgois <jeroen@jackjoe.be>
2024-12-28 13:37:53 +0100

    feat: trying out mini.nvim

--- users/jeroenb/config/nvim/lua/jeroen/plugins/nvim-surround.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/nvim-surround.lua
@@ -1,6 +1,7 @@
 return {
 	"kylechui/nvim-surround",
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	enabled = false,
 	event = "VeryLazy",
 	config = function()
 		require("nvim-surround").setup({
--- users/jeroenb/config/nvim/lua/jeroen/plugins/nvim-treesitter-textobjects.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/nvim-treesitter-textobjects.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
+	enabled = false,
 	lazy = true,
 	config = function()
 		require("nvim-treesitter.configs").setup({
