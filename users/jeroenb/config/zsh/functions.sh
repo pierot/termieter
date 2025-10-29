2ad5ab952b26445d9a81af87017fe3ab5519bf1e
Jeroen Bourgois <jeroen@jackjoe.be>
2025-10-29 16:33:51 +0100

    feat: gwt justified helper

--- users/jeroenb/config/zsh/functions.sh
+++ users/jeroenb/config/zsh/functions.sh
@@ -24,6 +24,15 @@ function sloc_ts {
   git ls-files | egrep '\.tsx?$' | xargs cat | sed '/^$/d' | sed '/^\s*#/d' | wc -l
 }
 
+function justified_gwt_init {
+  cp ../../../.env .env
+  cp -R ../../../priv/cert priv/cert
+  mix phx.gen.secret
+  mix deps.get
+  pnpm install
+  make styles
+}
+
 function take {
   mkdir -p $1
   cd $1
