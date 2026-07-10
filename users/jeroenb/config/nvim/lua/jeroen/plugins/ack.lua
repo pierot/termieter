97934729d09bf9c455cb34a95ef9f0622db8f71f
Jeroen Bourgois <jeroen@jackjoe.be>
2026-07-10 10:34:59 +0200

    fix: Ack again in nvim

--- users/jeroenb/config/nvim/lua/jeroen/plugins/ack.lua
+++ users/jeroenb/config/nvim/lua/jeroen/plugins/ack.lua
@@ -1,6 +1,9 @@
 return {
 	"mileszs/ack.vim",
 	event = "VeryLazy",
+	init = function()
+		vim.g.ackprg = "rg --vimgrep --smart-case"
+	end,
 	config = function()
 		vim.keymap.set("n", "<leader>a", ":Ack<space>")
 	end,
